643dd966c952096650ed15a66f1e47d6a3f583c3
Simon Ektjärn <debarquer@gmail.com>
2018-12-18 17:54:03 +0100

    Added piece prefab with lerp movement

---
+++ Lubos/Assets/Piece.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+
+    public void MoveToPosition(Vector3 target)
+    {
+        StartCoroutine(LerpToPosition(target));
+    }
+
+    IEnumerator LerpToPosition(Vector3 target)
+    {
+        Vector3 startPos = transform.position;
+
+        for (float i = 0; i < 1; i += Time.deltaTime)
+        {
+            transform.position = Vector3.Lerp(startPos, target, i * moveSpeed);
+
+            yield return null;
+        }
+    }
+}
